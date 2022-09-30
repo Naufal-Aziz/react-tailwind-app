bc22649f1bba944500ff4a000e7b5c7f5cdc78cd
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-09-30 11:53:23 +0700

    components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+} from "./components";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className="flex justify-center items-center sm:px-16 px-6">
+      <div className="xl:max-w-[1280px] w-full">
+        <Navbar />
+      </div>
+    </div>
 
-export default App
+    <div className="bg-primary flex justify-center items-start">
+      <div className="xl:max-w-[1280px] w-full">
+        <Hero />
+      </div>
+    </div>
+
+    <div className="bg-primary flex justify-center items-start sm:px-16 px-6">
+      <div className="xl:max-w-[1280px] w-full">
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
