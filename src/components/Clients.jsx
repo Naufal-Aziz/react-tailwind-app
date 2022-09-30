1cb7b507bee250c77a23b5682e38cf8fa03c8e80
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-09-30 16:00:00 +0700

    finish clients component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {airbnb, coinbase, binance, dropbox} from '../assets'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () =>  (
+    <div className='flex flex-col ss:flex-row justify-between gap-6 my-8 items-center'>
+      <img src={airbnb} alt="airbnb" className='h-[41px]'/>
+      <img src={coinbase} alt="coinbase" className='h-[41px]'/>
+      <img src={binance} alt="binance" className='h-[41px]'/>
+      <img src={dropbox} alt="dropbox" className='h-[41px]'/>
+    </div>
   )
-}
 
 export default Clients
